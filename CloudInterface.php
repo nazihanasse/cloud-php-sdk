ef262f440ba6ef3c475732de5fd6a1320c09ea93
alexander.bondars <alexander.bondars@cactussoft.biz>
2017-04-14 16:58:11 +0300

    Added base user-management functions. You can create and get user but without account activation. Created UserBundle and Application bundle.

---
+++ CloudInterface.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Skycentrics.
+ * Portal package
+ * User: Alexander Bondars <alexander.bondars@cactussoft.biz>
+ */
+
+namespace SkyCentrics\Cloud;
+
+
+interface CloudInterface
+{
+
+}

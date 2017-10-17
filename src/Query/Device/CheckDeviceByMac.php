7495a2bf86d797cb3746ec9091d04ccebd56bc05
alexandermashukevich <alexandr.mashukevich@cactussoft.ch>
2017-10-17 18:02:51 +0300

    Added check of device on mac

---
+++ src/Query/Device/CheckDeviceByMac.php
@@ -0,0 +1,65 @@
+<?php
+
+
+namespace SkyCentrics\Cloud\Query\User;
+
+
+use SkyCentrics\Cloud\Query\Device\AbstractDeviceQuery;
+use SkyCentrics\Cloud\Query\QueryInterface;
+use SkyCentrics\Cloud\Security\Account;
+use SkyCentrics\Cloud\Transport\Request\Request;
+use SkyCentrics\Cloud\Transport\Request\RequestInterface;
+use SkyCentrics\Cloud\Transport\Response\ResponseInterface;
+
+/**
+ * Class CheckDeviceByMac
+ * @package SkyCentrics\Cloud\Query\User
+ */
+class CheckDeviceByMac extends AbstractDeviceQuery
+{
+    /**
+     * @var string
+     */
+    protected $mac;
+
+    /**
+     * @var int
+     */
+    protected $type;
+
+    /**
+     * GetUserByEmail constructor.
+     * @param string $mac
+     * @param int $type
+     */
+    public function __construct(
+        string $mac,
+        int $type
+    )
+    {
+        $this->mac = $mac;
+        $this->type = $type;
+    }
+
+    /**
+     * @return RequestInterface
+     */
+    public function createRequest(): RequestInterface
+    {
+        return Request::createFromParams([
+            'path' => sprintf('/%s/', $this->getPath($this->type)),
+            'query' => [
+                'mac' => base64_encode($this->mac)
+            ]
+        ]);
+    }
+
+    /**
+     * @param ResponseInterface $response
+     */
+    public function mapResponse(ResponseInterface $response)
+    {
+
+        return true;
+    }
+}

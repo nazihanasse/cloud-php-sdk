e6b3bc71080272830269b7c957fe8a6cae85e6d9
alexandermashukevich <alexandr.mashukevich@cactussoft.ch>
2017-11-02 19:25:58 +0300

    Added method for creating a schedule

--- src/Query/Scheduler/CreateSchedule.php
+++ src/Query/Scheduler/CreateSchedule.php
@@ -4,7 +4,55 @@
 namespace SkyCentrics\Cloud\Query\Scheduler;
 
 
-class CreateSchedule
-{
+use SkyCentrics\Cloud\DTO\Schedule\CloudSchedule;
+use SkyCentrics\Cloud\Query\AbstractQuery;
+use SkyCentrics\Cloud\Transport\Request\MultiRequestInterface;
+use SkyCentrics\Cloud\Transport\Request\Request;
+use SkyCentrics\Cloud\Transport\Request\RequestInterface;
+use SkyCentrics\Cloud\Transport\Response\ResponseInterface;
 
+/**
+ * Class CreateSchedule
+ * @package SkyCentrics\Cloud\Query\Group
+ */
+class CreateSchedule extends AbstractQuery
+{
+    /**
+     * @var CloudSchedule
+     */
+    protected $schedule;
+
+    /**
+     * CreateGroup constructor.
+     * @param CloudSchedule $schedule
+     */
+    public function __construct(CloudSchedule $schedule)
+    {
+        $this->schedule = $schedule;
+    }
+
+    /**
+     * @return RequestInterface|MultiRequestInterface
+     */
+    public function createRequest(): RequestInterface
+    {
+        return Request::createFromParams([
+            'path' => '/schedules/',
+            'method' => Request::METHOD_POST,
+            'data' => $this->map($this->schedule)
+        ]);
+    }
+
+    /**
+     * @param ResponseInterface $response
+     * @return int|null
+     */
+    public function mapResponse(ResponseInterface $response)
+    {
+        $id = $response->getIdFromLocation();
+
+        $this->schedule->setId($id);
+
+        return $id;
+    }
 }

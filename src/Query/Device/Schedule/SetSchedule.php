2212878d148108d996ed39f5d6ff30b20431f891
alexandermashukevich <alexandr.mashukevich@cactussoft.ch>
2017-11-17 13:06:57 +0300

    Added a method for sending a schedule to the device

--- src/Query/Device/Schedule/SetSchedule.php
+++ src/Query/Device/Schedule/SetSchedule.php
@@ -3,8 +3,65 @@
 
 namespace SkyCentrics\Cloud\Query\Device\Schedule;
 
+use SkyCentrics\Cloud\DTO\Device\CloudDeviceID;
+use SkyCentrics\Cloud\DTO\Schedule\CloudSchedule;
+use SkyCentrics\Cloud\Mapper\AnnotationMapper;
+use SkyCentrics\Cloud\Transport\Request\MultiRequestInterface;
+use SkyCentrics\Cloud\Transport\Request\Request;
+use SkyCentrics\Cloud\Transport\Request\RequestInterface;
+use SkyCentrics\Cloud\Transport\Response\ResponseInterface;
+use SkyCentrics\Cloud\Query\Device\AbstractDeviceQuery;
 
-class SetSchedule
+
+class SetSchedule extends AbstractDeviceQuery
 {
+    /**
+     * @var CloudDeviceID
+     */
+    protected $cloudDeviceId;
 
+    /**
+     * @var CloudSchedule
+     */
+    protected $cloudSchedule;
+
+
+    /**
+     * GetDeviceDataQuery constructor.
+     * @param CloudDeviceID $cloudDeviceId
+     * @param CloudSchedule $cloudSchedule
+     */
+    public function __construct(CloudDeviceID $cloudDeviceId, CloudSchedule $cloudSchedule)
+    {
+        $this->cloudDeviceId = $cloudDeviceId;
+        $this->cloudSchedule = $cloudSchedule;
+
+    }
+
+    /**
+     * @return RequestInterface|MultiRequestInterface
+     */
+    public function createRequest(): RequestInterface
+    {
+
+        return $this->addSecurityHeaders(Request::createFromParams([
+            'path' => sprintf("/%s/%s/schedule", $this->getPath($this->cloudDeviceId->getType()), $this->cloudDeviceId->getId()),
+            'method' => Request::METHOD_POST,
+            'data' => $this->map($this->cloudSchedule)
+        ]));
+
+    }
+
+    /**
+     * @param ResponseInterface $response
+     * @return int|null
+     */
+    public function mapResponse(ResponseInterface $response)
+    {
+        $id = $response->getIdFromLocation();
+
+        $this->cloudSchedule->setId($id);
+
+        return $id;
+    }
 }

129b90da464698d209f01b3c0469f4e833d231b4
Alexandr Mashukevich <alexandr.mashukevich@cactussoft.biz>
2017-12-29 13:13:32 +0300

    Address added for group

--- src/DTO/CloudGroup.php
+++ src/DTO/CloudGroup.php
@@ -49,6 +49,48 @@ class CloudGroup implements CloudDTOInterface
     protected $target = 0;
 
     /**
+     * @var string
+     * @Property(key="address.street")
+     */
+    protected $street;
+
+    /**
+     * @var string
+     * @Property(key="address.city")
+     */
+    protected $city;
+
+    /**
+     * @var string
+     * @Property(key="address.state")
+     */
+    protected $state;
+
+    /**
+     * @var string
+     * @Property(key="address.zip")
+     */
+    protected $zip;
+
+    /**
+     * @var string
+     * @Property(key="address.country")
+     */
+    protected $country;
+
+    /**
+     * @var string
+     * @Property(key="address.lat")
+     */
+    protected $lat;
+
+    /**
+     * @var string
+     * @Property(key="address.long")
+     */
+    protected $long;
+
+    /**
      * CloudGroup constructor.
      * @param string $name
      * @param int $userId
@@ -123,5 +165,117 @@ class CloudGroup implements CloudDTOInterface
     public function setParent(int $parent)
     {
         $this->parent = $parent;
+    }
+
+    /**
+     * @return string
+     */
+    public function getStreet(): string
+    {
+        return $this->street;
+    }
+
+    /**
+     * @param string $street
+     */
+    public function setStreet(string $street): void
+    {
+        $this->street = $street;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCity(): string
+    {
+        return $this->city;
+    }
+
+    /**
+     * @param string $city
+     */
+    public function setCity(string $city): void
+    {
+        $this->city = $city;
+    }
+
+    /**
+     * @return string
+     */
+    public function getState(): string
+    {
+        return $this->state;
+    }
+
+    /**
+     * @param string $state
+     */
+    public function setState(string $state): void
+    {
+        $this->state = $state;
+    }
+
+    /**
+     * @return string
+     */
+    public function getZip(): string
+    {
+        return $this->zip;
+    }
+
+    /**
+     * @param string $zip
+     */
+    public function setZip(string $zip): void
+    {
+        $this->zip = $zip;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCountry(): string
+    {
+        return $this->country;
+    }
+
+    /**
+     * @param string $country
+     */
+    public function setCountry(string $country): void
+    {
+        $this->country = $country;
+    }
+
+    /**
+     * @return string
+     */
+    public function getLat(): string
+    {
+        return $this->lat;
+    }
+
+    /**
+     * @param string $lat
+     */
+    public function setLat(string $lat): void
+    {
+        $this->lat = $lat;
+    }
+
+    /**
+     * @return string
+     */
+    public function getLong(): string
+    {
+        return $this->long;
+    }
+
+    /**
+     * @param string $long
+     */
+    public function setLong(string $long): void
+    {
+        $this->long = $long;
     }
 }

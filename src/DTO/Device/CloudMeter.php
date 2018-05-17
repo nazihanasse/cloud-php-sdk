e1fa9eb61a7fd17333b4e94c9089db2c2692f5e3
Alexandr Mashukevich <alexandr.mashukevich@cactussoft.biz>
2018-05-17 18:05:20 +0300

    Changed meter data

--- src/DTO/Device/CloudMeter.php
+++ src/DTO/Device/CloudMeter.php
@@ -15,35 +15,35 @@ class CloudMeter extends AbstractCloudDevice
     /**
      * @var int
      *
-     * @Property(key="i", to_type="int")
+     * @Property(key="d.i", to_type="int")
      */
     protected $id = 0;
 
     /**
      * @var int
      *
-     * @Property(key="u", to_type="int")
+     * @Property(key="d.u", to_type="int")
      */
     protected $userId;
 
     /**
      * @var int
      *
-     * @Property(key="g", to_type="int")
+     * @Property(key="d.h", to_type="int")
      */
     protected $groupId = 0;
 
     /**
      * @var string
      *
-     * @Property(key="n", to_type="string")
+     * @Property(key="d.n", to_type="string")
      */
     protected $name;
 
     /**
-     * @var int
+     * @var string
      *
-     * @Property(key="m", to_type="string")
+     * @Property(key="d.m", to_type="string")
      */
     protected $mac = 0;
 
@@ -52,13 +52,13 @@ class CloudMeter extends AbstractCloudDevice
      *
      * @Property(key="t", to_type="int")
      */
-    protected $type = 0;
+    protected $type = DeviceTypeInterface::TYPE_METERS_0;
 
 
     /**
      * @var string
      *
-     * @Property(key="mdl")
+     * @Property(key="i.mdl")
      */
     protected $model;
 
@@ -66,14 +66,14 @@ class CloudMeter extends AbstractCloudDevice
     /**
      * @var string
      *
-     * @Property(key="mfg")
+     * @Property(key="i.mfg")
      */
     protected $manufacturer;
 
     /**
      * @var string
      *
-     * @Property(key="url")
+     * @Property(key="i.url")
      */
     protected $url;
 
@@ -97,7 +97,7 @@ class CloudMeter extends AbstractCloudDevice
     {
         $this->userId = $userId;
         $this->name = $name;
-        $this->type=  $type;
+        $this->type = $type;
         $this->mac = $mac;
         $this->model = $model;
         $this->groupId = $groupId;
@@ -106,32 +106,127 @@ class CloudMeter extends AbstractCloudDevice
     /**
      * @return int
      */
-    public function getLocation()
+    public function getId(): int
     {
-        return $this->location;
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     */
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return int
+     */
+    public function getUserId(): int
+    {
+        return $this->userId;
+    }
+
+    /**
+     * @param int $userId
+     */
+    public function setUserId(int $userId): void
+    {
+        $this->userId = $userId;
+    }
+
+    /**
+     * @return int
+     */
+    public function getGroupId(): int
+    {
+        return $this->groupId;
+    }
+
+    /**
+     * @param int $groupId
+     */
+    public function setGroupId(int $groupId): void
+    {
+        $this->groupId = $groupId;
     }
 
     /**
      * @return string
      */
-    public function getAuth()
+    public function getName(): string
     {
-        return $this->auth;
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName(string $name): void
+    {
+        $this->name = $name;
     }
 
     /**
      * @return string
      */
-    public function getManufacturer()
+    public function getMac(): string
+    {
+        return $this->mac;
+    }
+
+    /**
+     * @param string $mac
+     */
+    public function setMac(string $mac): void
+    {
+        $this->mac = $mac;
+    }
+
+    /**
+     * @return int
+     */
+    public function getType(): int
+    {
+        return $this->type;
+    }
+
+    /**
+     * @param int $type
+     */
+    public function setType(int $type): void
+    {
+        $this->type = $type;
+    }
+
+    /**
+     * @return string
+     */
+    public function getModel(): string
+    {
+        return $this->model;
+    }
+
+    /**
+     * @param string $model
+     */
+    public function setModel(string $model): void
+    {
+        $this->model = $model;
+    }
+
+    /**
+     * @return string
+     */
+    public function getManufacturer(): string
     {
         return $this->manufacturer;
     }
 
-
     /**
      * @param string $manufacturer
      */
-    public function setManufacturer(string $manufacturer)
+    public function setManufacturer(string $manufacturer): void
     {
         $this->manufacturer = $manufacturer;
     }
@@ -139,7 +234,7 @@ class CloudMeter extends AbstractCloudDevice
     /**
      * @return string
      */
-    public function getUrl()
+    public function getUrl(): string
     {
         return $this->url;
     }
@@ -147,9 +242,8 @@ class CloudMeter extends AbstractCloudDevice
     /**
      * @param string $url
      */
-    public function setUrl(string $url)
+    public function setUrl(string $url): void
     {
         $this->url = $url;
     }
-
 }
